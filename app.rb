af0b10507636b0b0c93a49f14d76989bfb0e0f83
Greg Morgan <gregm9999@gmail.com>
2018-12-08 05:08:12 +0000

    Done.

--- app.rb
+++ app.rb
@@ -12,13 +12,14 @@ module FormsLab
     end
 
     post '/pirates' do
-        params["pirate"]["ships"].each do |ship_data|
-          Ship.new(ship_data["name"], ship_data["type"], ship_data["booty"])
-        end
-        @pirate = Pirate.new(params["pirate"]["name"], params["pirate"]["height"], params["pirate"]["weight"])
-        @ships = Ship.all
-        erb :'pirates/show'
+      params["pirate"]["ships"].each do |data|
+        Ship.new(data["name"], data["type"], data["booty"])
       end
+      @pirate=Pirate.new(params["pirate"]["name"], params["pirate"]["height"], params["pirate"]["weight"])
+      @ships=Ship.all
+      erb :'pirates/show'
+    end
+
 
   end
 end
