e9cc19a3a886d748296131344eb9c09dcdfdb77f
Greg Morgan <gregm9999@gmail.com>
2018-12-08 03:11:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,22 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      params[pirate][ships].each do |data|
+        Ship.new(data[name], data[type], data[booty])
+      end
+      @pirate=Pirate.new(params[pirate][name], params[pirate][name], params[pirate][name])
+      @ships=Ship.all
+      erb :'pirates/show'
+    end
 
   end
 end
--- config.ru
+++ config.ru
@@ -1,2 +1,14 @@
 require './app'
 run FormsLab::App
+
+# class Sinatra::Base
+#   set :show_exceptions => false
+#
+#   error { |err|
+#     Rack::Response.new(
+#       [{'error' => err.message}.to_json],
+#       500,
+#       {'Content-type' => 'application/json'}
+#     ).finish
+#   }
+# end
