d092d82b5dfceaf6f530f53816e3c7eea7063329
Greg Morgan <gregm9999@gmail.com>
2018-12-08 04:59:31 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,21 @@
 class Pirate
+
+  attr_accessor :name, :height, :weight
+  @@all=[]
+
+  def initialize(name, height, weight )
+    @name=name
+    @height=height
+    @weight=weight
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all=[]
+  end
+
 end
