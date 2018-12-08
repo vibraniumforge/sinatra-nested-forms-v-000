f30eee7944ceb65d389f5c24fae479cff2685f65
Greg Morgan <gregm9999@gmail.com>
2018-12-08 00:12:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,13 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :new
+    end
+
+    post '/pirates' do
+      erb :show
+    end
 
   end
 end
